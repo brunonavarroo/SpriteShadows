9f3814247ed73866e695761138807b99b62ae806
flamencoBusiness <53520681+flamencoBusiness@users.noreply.github.com>
2020-04-18 11:31:11 +0200

    Add files via upload

---
+++ SpriteShadow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteShadow : MonoBehaviour
+{
+    public Vector2 offset = new Vector2(-3, -3);
+
+    private SpriteRenderer sprRndCaster;
+    private SpriteRenderer sprRndShadow;
+
+    private Transform transCaster;
+    private Transform transShadow;
+
+    public Material shadowMaterial;
+    public Color shadowColor;
+
+    private void Start()
+    {
+        transCaster = transform;
+        transShadow = new GameObject().transform;
+        transform.parent = transCaster;
+        transShadow.gameObject.name = "shadow";
+        transShadow.localRotation = Quaternion.identity; // weird
+
+        sprRndCaster = GetComponent<SpriteRenderer>();
+        sprRndShadow = transShadow.gameObject.AddComponent<SpriteRenderer>();
+
+        sprRndShadow.material = shadowMaterial;
+        sprRndShadow.color = shadowColor;
+        sprRndShadow.sortingLayerName = sprRndCaster.sortingLayerName;
+        sprRndShadow.sortingOrder = sprRndCaster.sortingOrder - 1;
+    }
+
+    private void LateUpdate()
+    {
+        transShadow.position = new Vector2(transCaster.position.x + offset.x,
+            transCaster.position.y + offset.y);
+        sprRndShadow.sprite = sprRndCaster.sprite;
+    }
+}
